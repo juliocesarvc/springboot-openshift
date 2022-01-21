d649213e8286af670b53234f4b4781666f5bda14
Julio Cesar <julio@julio-Precision-M2800>
2022-01-21 13:20:36 -0600

    Se agregaron properties y libreria openshift

--- src/main/java/com/learn/springbootkubernetes/Controller.java
+++ src/main/java/com/learn/springbootkubernetes/Controller.java
@@ -1,12 +1,35 @@
 package com.learn.springbootkubernetes;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+
+    @Value("${example.property.1}")
+    private String exampleProperty1;
+
+    @Value("${example.property.2}")
+    private String exampleProperty2;
+
+
+    @Value("${property.1}")
+    private String property1;
+
+    @Value("${property.2}")
+    private String property2;
+
+    @Value("${property.3}")
+    private String property3;
+
+
+
     @RequestMapping("/")
     public String index() {
-        return "Hello Spring-Boot app successfully deployed and running on Minishift";
+        return "Hello Spring-Boot app successfully deployed and running on Minishift |"
+            + exampleProperty1 + "|" + exampleProperty2
+            + "|||" + property1 + "|" + property2 + "|" + property3 + "|||"
+            ;
     }
 }
